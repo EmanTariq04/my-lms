bbaf8ca4a8659200d7e5e27a41797ce77601db54
Eman Tariq <emantariq.1004@gmail.com>
2025-03-15 23:23:01 +0500

    integrating backend

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -2,8 +2,9 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="min-h-screen bg-background">
       <Hero/>
+      courses will be shown here
     </div>
   );
 }
