1c20d659b9eb763596d4726d0e1b51f562e4b48a
Eman Tariq <emantariq.1004@gmail.com>
2025-03-27 22:30:31 +0500

    course page

--- app/(dashboard)/dashboard/courses/[courseId]/page.tsx
+++ app/(dashboard)/dashboard/courses/[courseId]/page.tsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import getCourseById from "@/sanity/lib/courses/getCourseById"
+import { redirect } from "next/navigation"
 
-function CoursePage() {
+interface CoursePageProps {
+  params: Promise<{
+    courseId: string;
+  }>
+}
+
+async function CoursePage({ params }: CoursePageProps) {
+  const { courseId } = await params;
+  const course = await getCourseById(courseId)
+
+  if (!course) {
+    return redirect("/")
+  }
+
+
+  if (course.modules?.[0].lessons?.[0]?._id) {
+    return redirect(
+      `dashboard/courses/${courseId}/lessons/${course.modules[0].lessons[0]._id}`
+    )
+  }
   return (
     <div>CoursePage</div>
   )
