64f5b5c05dc224fce51c942b3765b01704588f9b
Eman Tariq <emantariq.1004@gmail.com>
2025-03-28 22:23:28 +0500

    Loom Embed

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import ReactPlayer from "react-player";
+import dynamic from "next/dynamic";
+
+const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
 
 interface VideoPlayerProps {
   url: string;
